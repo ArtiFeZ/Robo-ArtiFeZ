cf842ac5d3ccd6a7f16a02da9b97ad5101f7c4de
VideroDesigns <maxayprivate@gmail.com>
2020-11-03 13:57:29 +0530

    Basic Setup

--- main.py
+++ main.py
@@ -12,5 +12,20 @@ bot = commands.Bot(
     intents=intents,
 )
 
+ArtiFeZ_guild_id = 715126942294343700
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.display_name + '#' + bot.user.discriminator}!")
+    print(f"ArtiFeZ members: {len(bot.guilds)}")
+    print(f"Average latency: {round(int(bot.latency * 1000))}ms")
+
+@bot.event
+async def on_message(message : discord.Message):
+    if message.guild.id != ArtiFeZ_guild_id:
+        return
+    else:
+        pass
 
 
+bot.run(str(token))
