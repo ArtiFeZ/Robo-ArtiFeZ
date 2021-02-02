cf6f0084cc426d99ddc3d6cc7c22c16c3d63630c
Namz <manit.op@outlook.com>
2021-02-02 17:21:50 +0530

    Added AFK Command for Team Members

--- cogs/extra.py
+++ cogs/extra.py
@@ -40,6 +40,35 @@ class Extra(commands.Cog):
             self.bot.unload_extension("cogs." + name)
             self.bot.load_extension("cogs." + name)
             await ctx.send(embed=qEmbed(title=f"Successfully reloaded the `{name}` extension."))
+            
+    afkdict = {}
+    @commands.command(name = "afk", brief = "Away From Keyboard",
+                    description = "I'll give you the afk status and if someone pings you before you come back, I'll tell "
+                                  "them that you are not available. You can add your own afk message!")
+    @commands.has_role(TeamRoleID)
+    async def afk(ctx, message = "AFK"):
+        global afkdict
+
+        if ctx.message.author in afkdict:
+            afkdict.pop(ctx.message.author)
+            await ctx.send('Welcome back! You are no longer afk.')
+
+        else:
+            afkdict[ctx.message.author] = message
+            await ctx.send(f"{ctx.author.mention} is now AFK: {message}")
+
+
+    @commands.Cog.listener()
+    async def on_message(message):
+        global afkdict
+
+        for member in message.mentions:  
+            if member != message.author:  
+                if member in afkdict:  
+                    afkmsg = afkdict[member]  
+                    await message.channel.send(f"! {member} is afk. {afkmsg}")
+        await bot.process_commands(message)
+
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
